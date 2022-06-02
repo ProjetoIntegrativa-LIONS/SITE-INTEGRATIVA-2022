66696d1124e6aaedacd261aae7d3dc81f08ad40d
Leonardo Margoti Meister <leozinhomm1@gmail.com>
2022-06-01 23:28:28 -0300

    Att

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 
 from Diretorio_home.home import bp_home
 from funcoes import Funcoes
-#from dir_login.login import bp_login
+
 
 app = Flask(__name__) 
 
@@ -13,21 +13,18 @@ app.secret_key = os.urandom(12).hex()
 
 app.register_blueprint(bp_home)
 
-
-
-
 @app.errorhandler(404)
 def rotaErro404(error):
     return render_template("form_404.html"), 404
 
 #Antes do primeiro request
-@app.before_first_request
+@app.before_request
 def before_request():
     session.permanent = True
-    user_agent = request.headers.get('User-Agent')
-    dispositivo = Funcoes.PegarTipoDispositivo(user_agent)    
-    session['dispositivo'] = dispositivo
-    print(dispositivo)   
+    user_agent = request.headers.get('User-Agent')    
+    session['dispositivo'] = Funcoes.PegarTipoDispositivo(user_agent)    
+    print(session['dispositivo'])   
+
 
 
 if __name__ == "__main__":
