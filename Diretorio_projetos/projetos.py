29386761f9cceada3183296d35bc98030dc5e6d3
Leonardo Margoti Meister <leozinhomm1@gmail.com>
2022-06-07 23:00:16 -0300

    Ajustes rota / de projetos

--- Diretorio_projetos/projetos.py
+++ Diretorio_projetos/projetos.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template , request, session
 bp_projetos = Blueprint('projetos',__name__, url_prefix="/projetos", template_folder= 'templates')
 
 @bp_projetos.route("/")
-def rotaHome():
+def projetos():
     dispositivo = session['dispositivo']
     if dispositivo == "desktop":
         return render_template("projetosDesktop.html")
