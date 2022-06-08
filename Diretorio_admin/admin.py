061a6a288ce137112c228b77e74d7dcd125d0d44
Leonardo Margoti Meister <leozinhomm1@gmail.com>
2022-06-08 16:07:07 -0300

    att

--- Diretorio_admin/admin.py
+++ Diretorio_admin/admin.py
@@ -9,23 +9,27 @@ bp_admin = Blueprint('admin',__name__, url_prefix="/admin", template_folder= 'te
 @bp_admin.route("/", methods = ['POST' , 'GET'] )
 @validarSessao
 def homeAdmin():
-    return render_template("InicioAdminDesktop.html" )
+    tela = 1
+    return render_template("InicioAdminDesktop.html" ,tela=1)
 
 
 @bp_admin.route("/AdmQuemSomos" )
 @validarSessao
 def AdmQuemSomos():
-    return render_template("QuemSomosAdminDesktop.html")
+    tela = 1
+    return render_template("QuemSomosAdminDesktop.html",tela=2)
 
 @bp_admin.route("/AdmProjetos" )
 @validarSessao
 def AdmProjetos():
-    return render_template("ProjetosAdminDesktop.html")
+    tela = 1
+    return render_template("ProjetosAdminDesktop.html",tela=3)
 
 @bp_admin.route("/AdmContato" )
 @validarSessao
 def AdmContato():
-    return render_template("ContatoAdminDesktop.html")
+    tela = 1
+    return render_template("ContatoAdminDesktop.html",tela=4)
 
 
 
