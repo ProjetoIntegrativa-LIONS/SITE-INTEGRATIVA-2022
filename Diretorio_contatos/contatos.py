dba1623d046709195c6d40d1acc659091ffe2919
Leonardo Margoti Meister <leozinhomm1@gmail.com>
2022-06-07 23:05:26 -0300

    Add BP contatos e html contatos

---
+++ Diretorio_contatos/contatos.py
@@ -0,0 +1,9 @@
+from functools import wraps
+from flask import Blueprint, render_template , redirect , request, url_for, session, jsonify
+from funcoes import Funcoes , LogEnum
+
+bp_contatos = Blueprint('contatos',__name__, url_prefix="/contatos", template_folder= 'templates')
+
+@bp_contatos.route("/" )
+def contatos():
+    return render_template('contatosDesktop.html')
