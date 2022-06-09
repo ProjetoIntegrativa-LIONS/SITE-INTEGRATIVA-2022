87731f813ff81ba027dbc7722de927c6f20b826b
Marcos Lima <developermarcos@outlook.com>
2022-06-09 10:57:33 -0300

    Nome menu e tela admin

--- Diretorio_admin/admin.py
+++ Diretorio_admin/admin.py
@@ -5,31 +5,37 @@ from Diretorio_login.login import validarSessao
 
 bp_admin = Blueprint('admin',__name__, url_prefix="/admin", template_folder= 'templates')
 
+discionarioTelas = {
+    'inicio' : 'inicio',
+    'quemSomos' : 'quemSomos',
+    'projetos' : 'projeto',
+    'contato' : 'contato'
+}
 
 @bp_admin.route("/", methods = ['POST' , 'GET'] )
 @validarSessao
 def homeAdmin():
     tela = 1
-    return render_template("InicioAdminDesktop.html" ,tela=1)
+    return render_template("InicioAdminDesktop.html" , tela=discionarioTelas.get('inicio'))
 
 
 @bp_admin.route("/AdmQuemSomos" )
 @validarSessao
 def AdmQuemSomos():
     tela = 1
-    return render_template("QuemSomosAdminDesktop.html",tela=2)
+    return render_template("QuemSomosAdminDesktop.html", tela=discionarioTelas.get('quemSomos'))
 
 @bp_admin.route("/AdmProjetos" )
 @validarSessao
 def AdmProjetos():
     tela = 1
-    return render_template("ProjetosAdminDesktop.html",tela=3)
+    return render_template("ProjetosAdminDesktop.html", tela=discionarioTelas.get('projetos'))
 
 @bp_admin.route("/AdmContato" )
 @validarSessao
 def AdmContato():
     tela = 1
-    return render_template("ContatoAdminDesktop.html",tela=4)
+    return render_template("ContatoAdminDesktop.html", tela=discionarioTelas.get('contato'))
 
 
 
