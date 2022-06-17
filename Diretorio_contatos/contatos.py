6b156da0625de17729ae8ba28611cc95f4a2eb47
Leonardo Margoti Meister <leozinhomm1@gmail.com>
2022-06-17 20:08:42 -0300

    config de passar dados para tora contatos home

--- Diretorio_contatos/contatos.py
+++ Diretorio_contatos/contatos.py
@@ -1,9 +1,12 @@
 from functools import wraps
 from flask import Blueprint, render_template , redirect , request, url_for, session, jsonify
 from funcoes import Funcoes , LogEnum
+from Dominio_project.ControlContatos import Contato,ControlContato
 
 bp_contatos = Blueprint('contatos',__name__, url_prefix="/contatos", template_folder= 'templates')
 
 @bp_contatos.route("/" )
 def contatos():
-    return Funcoes.CarregarRota(caminhoDesktop="contatosDesktop.html",caminhoMobile="contatosMobile.html")
+    controlador = ControlContato();
+    dados = controlador.SelectAll();
+    return Funcoes.CarregarRota(caminhoDesktop="contatosDesktop.html",caminhoMobile="contatosMobile.html", dados=dados)
