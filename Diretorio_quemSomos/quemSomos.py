6c0862897101133174705933690d3d8dcb21fa1a
Leonardo Margoti Meister <leozinhomm1@gmail.com>
2022-06-17 20:20:50 -0300

    correcao de passagem de dados para rota quemSomos

--- Diretorio_quemSomos/quemSomos.py
+++ Diretorio_quemSomos/quemSomos.py
@@ -1,9 +1,12 @@
 from functools import wraps
+import imp
 from flask import Blueprint, render_template , redirect , request, url_for, session, jsonify
 from funcoes import Funcoes , LogEnum
-
+from Dominio_project.ControlQuemSomos import QuemSomos,ControlQuemSomos
 bp_quemSomos = Blueprint('quemSomos',__name__, url_prefix="/quemSomos", template_folder= 'templates')
 
 @bp_quemSomos.route("/" )
 def quemSomos():
-    return Funcoes.CarregarRota('QuemSomosDesktop.html', 'QuemSomosMobile.html')    
+    controlador = ControlQuemSomos()
+    dados = controlador.SelectAll();
+    return Funcoes.CarregarRota('QuemSomosDesktop.html', 'QuemSomosMobile.html',dados=dados)    
