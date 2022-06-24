4bb2744697404e40d63b26319c368b8c32504ae5
Leonardo Margoti Meister <leozinhomm1@gmail.com>
2022-06-24 14:49:56 -0300

    att

--- Diretorio_projetos/projetos.py
+++ Diretorio_projetos/projetos.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template 
+from flask import Blueprint, render_template, request
 from Dominio_project.ControlProjeto import Projeto,ControlProjeto
 
 bp_projetos = Blueprint('projetos',__name__, url_prefix="/projetos", template_folder= 'templates')
@@ -9,3 +9,12 @@ def projetos():
     controlador = ControlProjeto();
     dados = controlador.SelectAll();        
     return render_template("projetosDesktop.html", dados=dados, tela=nomeTela)
+
+@bp_projetos.route("/viewProjeto", methods = ['POST'])
+def viewProjeto():
+    if request.method == "POST":    
+        nomeTela = "projetos"
+        id = request.form['idProjeto']
+        controlador = ControlProjeto();
+        dados = controlador.SelectId(id);
+    return render_template("projetoDesktop.html", dados=dados, tela=nomeTela)
